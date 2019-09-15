172425a7ae093ae6de4dc43e9f585bf77e33d206
fps <mista.tapas@gmx.net>
2019-09-15 06:04:30 +0000

    add signal handling; do pwm outside of process()

--- jack_led_peak.cc
+++ jack_led_peak.cc
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <boost/program_options.hpp>
 #include <jack/jack.h>
+#include <signal.h>
 
 namespace po = boost::program_options;
 
@@ -33,14 +34,25 @@ float blink_cycle = 0.0;
 float green_old_max = 0.0;
 float red_old_max = 0.0;
 
+bool red_led_do_hold = false;
+bool red_led_do_blink = false;
+
+bool quit = false;
+
+extern "C" 
+{
+    void signal_handler(int signo)
+    {
+        quit = true;
+    }
+}
+
 int
 process (jack_nframes_t nframes, void *arg)
 {
     const float samplerate = (float)jack_get_sample_rate(jack_client);
 
     const float time_passed = (float)nframes / samplerate;
-
-    float draw = (float)rand()/(float)RAND_MAX;
 
     float current_max = 0;
     for (unsigned int index = 0; index < jack_ports.size(); ++index)
@@ -69,8 +81,11 @@ process (jack_nframes_t nframes, void *arg)
         time_since_red_led_blink_triggered = 0;
     }
 
+#if 0
     if (time_since_red_led_blink_triggered < red_led_hysteresis_secs)
     {
+        red_led_do_blink = true;
+#if 0
         if (blink_cycle < 0.5)
         {
             gpiod_line_set_value(red_led_line, 1);
@@ -79,9 +94,15 @@ process (jack_nframes_t nframes, void *arg)
         {
             gpiod_line_set_value(red_led_line, 0);
         }
+#endif
     }
     else
     {
+        if (time_since_red_led_triggered < red_led_hysteresis_secs)
+        {
+            red_led_do_hold = true;
+        }
+#if 0
         if (time_since_red_led_triggered < red_led_hysteresis_secs || draw < (red_old_max - green_led_threshold_gain) / (red_led_threshold_gain - green_led_threshold_gain))
         {
             gpiod_line_set_value(red_led_line, 1);
@@ -90,8 +111,10 @@ process (jack_nframes_t nframes, void *arg)
         {
             gpiod_line_set_value(red_led_line, 0);
         }
+#endif
     }
- 
+
+#if 0 
     if (green_old_max > green_led_threshold_gain || draw < (green_old_max / green_led_threshold_gain))
     {
         gpiod_line_set_value(green_led_line, 1);
@@ -100,6 +123,8 @@ process (jack_nframes_t nframes, void *arg)
     {
         gpiod_line_set_value(green_led_line, 0);
     }
+#endif
+#endif
 
     time_since_red_led_triggered += time_passed;
     time_since_red_led_blink_triggered += time_passed;
@@ -126,6 +151,9 @@ int main(int ac, char *av[])
     float green_led_threshold_dbfs;
     float red_led_threshold_dbfs;
     float red_led_blink_threshold_dbfs;
+    float pwm_frequency_hz;
+
+    std::vector<std::string> gpiod_led_offsets_string;
 
     po::options_description desc("Allowed options");
     desc.add_options()
@@ -134,6 +162,7 @@ int main(int ac, char *av[])
         ("jack-server-name,e", po::value<std::string>(&jack_server_name)->default_value("default"), "The jack server name to use")
         ("jack-number-of-input-ports,n", po::value<int>(&jack_number_of_input_ports)->default_value(2), "The number of input ports to watch")
         ("gpiod-chip-device-path,d", po::value<std::string>(&gpiod_chip_device_path)->default_value("/dev/gpiochip0"), "The path of the gpiochip device to use")
+        // ("gpiod-chip-led-offsets,o", po::value<std::vector<std::string>>(&gpiod_led_offsets_string)->default_value({"23,18"}), "Comma-separated list of gpiod line offsets to specify green,red LEDs")
         ("gpiod-green-led-offset,g", po::value<int>(&gpiod_green_led_offset)->default_value(23), "The libgpiod line offset to use for the green indicator LED")
         ("gpiod-red-led-offset,r", po::value<int>(&gpiod_red_led_offset)->default_value(18), "The libgpiod line offset to use for the red indicator LED")
         ("green-led-threshold-dbfs,t", po::value<float>(&green_led_threshold_dbfs)->default_value(-18.0), "The full saturation threshold for the green LED (note: this is not using oversampling, thus the value will be underestimated)")
@@ -143,6 +172,7 @@ int main(int ac, char *av[])
         ("red-led-blink-frequency-hz,f", po::value<float>(&red_led_blink_frequency_hz)->default_value(10), "The red LED blinking frequency (approximate)")
         ("green-led-falloff-time-constant-secs,z", po::value<float>(&green_led_falloff_time_constant_secs)->default_value(0.1), "The green LED's time for the exponential falloff to drop to half the peak value")
         ("red-led-falloff-time-constant-secs,x", po::value<float>(&red_led_falloff_time_constant_secs)->default_value(0.4), "The red LED's time for the exponential falloff to drop to half the peak value")
+        ("pwm-frequency_hz,p", po::value<float>(&pwm_frequency_hz)->default_value(10000), "The frequency with which to perform PWM (approximate)")
     ;
 
     po::variables_map vm;
@@ -198,13 +228,46 @@ int main(int ac, char *av[])
         }
     }
 
+    signal(SIGTERM, signal_handler);
+    signal(SIGINT, signal_handler);
+
     if (jack_activate(jack_client))
     {
         std::cout << "Failed to activate jack_client" << std::endl;
         return 1;
     }
 
-    sleep(-1);
+    const unsigned int pwm_usecs = (unsigned int)(1000000 * 1.0  / pwm_frequency_hz);
+
+    while(false == quit)
+    {
+        const float draw1 = (float)rand()/(float)RAND_MAX;
+        const float draw2 = (float)rand()/(float)RAND_MAX;
+
+        if (draw1 < (green_old_max / green_led_threshold_gain))
+        {
+            gpiod_line_set_value(green_led_line, 1);
+        }
+        else
+        {
+            gpiod_line_set_value(green_led_line, 0);
+        }
+
+        if (draw2 < ((red_old_max - green_led_threshold_gain) / (red_led_threshold_gain)))
+        {
+            gpiod_line_set_value(red_led_line, 1);
+        }
+        else
+        {
+            gpiod_line_set_value(red_led_line, 0);
+        }
+
+        usleep(pwm_usecs);
+    }
+
+    jack_deactivate(jack_client);
+
+    std::cout << "Bye." << std::endl;
 
     return 0;
 }
--- makefile
+++ makefile
@@ -1,2 +1,2 @@
 jack_led_peak: jack_led_peak.cc
-	g++ -O3 $(CXXFLAGS) -Wall -o jack_led_peak jack_led_peak.cc -lgpiod -ljack -lboost_program_options
+	g++ -std=c++11 -O3 $(CXXFLAGS) -Wall -o jack_led_peak jack_led_peak.cc -lgpiod -ljack -lboost_program_options
